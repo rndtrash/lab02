8830b965b0ae3ac7c5bba2749be967a3693f1201
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2023-10-21 08:31:59 +0300

    Ask for a name

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,7 +7,11 @@ int main(int argc, char **argv)
 	(void) argc;
 	(void) argv;
 
-	cout << "Hello, world!" << endl;
+	std::string name;
+	cout << "What's your name? ";
+	cin >> name;
+	
+	cout << "Hello, world from " << name << "!" << endl;
 
 	return 0;
 }
