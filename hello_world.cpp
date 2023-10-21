bbd5e553ae7a39771c67d7300d00faab5ae39c7d
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2023-10-21 08:39:06 +0300

    Fix the code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char **argv)
 {
 	(void) argc;
 	(void) argv;
 
 	std::string name;
-	cout << "What's your name? ";
-	cin >> name;
+	std::cout << "What's your name? ";
+	std::cin >> name;
 	
-	cout << "Hello, world from " << name << "!" << endl;
+	std::cout << "Hello, world from " << name << "!" << std::endl;
 
 	return 0;
 }
